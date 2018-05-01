8144607672029a8308eac2b7c1d426b1ddb44368
Unknown <trevor.vhoof@gmail.com>
2018-05-01 18:35:34 +0200

    Added fileutil extension to deal with backslash to forward slash conversions. Also contains a FileSystemWatcher that wraps QFileSystemWatcher in the same way.

--- SqrMelon/fileutil.py
+++ SqrMelon/fileutil.py
@@ -1,16 +1,35 @@
+from qtutil import *
 import os, stat
 from contextlib import contextmanager
+
+
+def exists(filePath):
+    return os.path.exists(filePath.replace('\\', '/'))
 
 
 @contextmanager
 def edit(filePath, flag='w'):
     """
-    Open a file for writing and forces it to be writable.
+    Forces a file to be writable & opens it.
     :type filePath: str
     :param str flag: IO mode, one of r(ead), w(rite), x(create), a(ppend), suffix with + for read-write, suffix with b for binary IO.
     """
+    filePath = filePath.replace('\\', '/')
     if os.path.exists(filePath):
         os.chmod(filePath, stat.S_IWRITE)
+    fh = open(filePath, flag)
+    yield fh
+    fh.close()
+
+
+@contextmanager
+def read(filePath, flag='r'):
+    """
+    Alternative to open() that enforces forward slashes.
+    :type filePath: str
+    :param str flag: IO mode, one of r(ead), w(rite), x(create), a(ppend), suffix with + for read-write, suffix with b for binary IO.
+    """
+    filePath = filePath.replace('\\', '/')
     fh = open(filePath, flag)
     yield fh
     fh.close()
@@ -30,3 +49,42 @@ def create(filePath):
     if not os.path.basename(filePath):
         return
     open(filePath, 'w').close()
+
+
+class FileSystemWatcher(object):
+    def __init__(self, *args):
+        initialPaths = tuple()
+        if args and hasattr(args[0], '__iter__'):
+            initialPaths = args[0]
+            if len(args) > 1:
+                args = (args[1],)
+            else:
+                args = tuple()
+        self.__internal = QFileSystemWatcher(*args)
+        self.addPaths(initialPaths)
+        self.fileChanged = Signal()
+        self.directoryChanged = Signal()
+        self.__internal.fileChanged.connect(self.__forwardFileChanged)
+        self.__internal.directoryChanged.connect(self.__forwardDirectoryChanged)
+
+    def __forwardFileChanged(self, path):
+        self.fileChanged.emit(path.replace('\\', '/'))
+
+    def __forwardDirectoryChanged(self, path):
+        self.directoryChanged.emit(path.replace('\\', '/'))
+
+    def addPath(self, path):
+        path = path.replace('\\', '/')
+        self.__internal.addPath(path)
+
+    def addPaths(self, paths):
+        paths = [path.replace('\\', '/') for path in paths]
+        self.__internal.addPaths(paths)
+
+    def removePath(self, path):
+        path = path.replace('\\', '/')
+        self.__internal.removePath(path)
+
+    def removePaths(self, paths):
+        paths = [path.replace('\\', '/') for path in paths]
+        self.__internal.removePaths(paths)
