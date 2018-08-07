aef9ffec513fde89e3ce53736fc18aef04728437
Unknown <trevor.vhoof@gmail.com>
2018-08-07 17:29:39 +0200

    How is this software so bad

--- SqrMelon/defaultproject/Templates/default/blurfooter.glsl
+++ SqrMelon/defaultproject/Templates/default/blurfooter.glsl
@@ -1,19 +1,4 @@
-<<<<<<< Updated upstream:SqrMelon/defaultproject/Templates/master/directionalblurrefl.glsl
-#version 410
-
-out vec4 c;
-
-uniform vec2 uResolution;
-
-uniform sampler2D uImages[1];
-uniform vec2 uDirection;
-uniform float uBlurSize;
-
 const float e[7]=float[7](0.0205,.0855,.232,.324,.232,.0855,.0205);
-uniform float g; // use input reflection roughness for blur kernel?
-=======
-const float e[7]=float[7](0.0205,.0855,.232,.324,.232,.0855,.0205);
->>>>>>> Stashed changes:SqrMelon/defaultproject/Templates/default/blurfooter.glsl
 
 void main()
 {
