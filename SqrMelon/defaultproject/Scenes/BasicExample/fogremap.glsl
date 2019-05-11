3d952003d77a056eee3d993bf230b18dc2fa2a46
Unknown <trevor.vhoof@gmail.com>
2019-05-11 21:17:09 +0900

    Improved the example projects

--- SqrMelon/defaultproject/Scenes/BasicExample/fogremap.glsl
+++ SqrMelon/defaultproject/Scenes/BasicExample/fogremap.glsl
@@ -5,5 +5,8 @@ smoothstep it for no fog at a certain region, or raise it to a small power (< 1)
 */
 float FogRemap(float fog)
 {
-    return fog;
+    return fog; // no change
+    // return pow(fog, 3.0); // better view distance
+    // return smoothstep(0.5, 1.0, fog); // fog only kicks in halfway towards the FAR plane
+    // return pow(fog, 0.1); // really foggy
 }
--- SqrMelon/defaultproject/Templates/default/noise.glsl
+++ SqrMelon/defaultproject/Templates/default/noise.glsl
@@ -1,4 +1,13 @@
+// This is a basic noise look up texture
+// it has perlin noise with different zoom
+// levels in every color channel.
 void main()
 {
-    outColor0=vec4(perlin(gl_FragCoord.xy/uResolution,8.0,15,2.0,0.5));
+    vec2 uv = gl_FragCoord.xy/uResolution;
+    outColor0 = vec4(
+        perlin(uv,  8.0, 7, 2.0, 0.5),
+        perlin(uv, 16.0, 7, 2.0, 0.5),
+        perlin(uv, 32.0, 7, 2.0, 0.5),
+        perlin(uv, 64.0, 7, 2.0, 0.5)
+    );
 }
