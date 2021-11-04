9d23422f4f741ff354d3755ff068e6d233e789a8
Trevor <trevor@applied.co>
2021-11-04 20:29:38 +0900

    Fix import err

--- SqrMelon/build/generate.py
+++ SqrMelon/build/generate.py
@@ -1,10 +1,10 @@
-from pycompat import *
 import os
 import sys
 import struct
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pycompat import *
 from build.codeoptimize import optimizeText
 from fileutil import FilePath
 from util import parseXMLWithIncludes, SCENE_EXT, currentScenesDirectory
