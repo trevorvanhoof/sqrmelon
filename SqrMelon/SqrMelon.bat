4bf7b9e9f97d6a8000310e6f8972eeb16d7a6402
David Cañadas Mazo <david.canadas.mazo@gmail.com>
2024-08-25 00:58:43 +0200

    Use latest version of Python 3 instead of hardcoded 3.11.

--- SqrMelon/SqrMelon.bat
+++ SqrMelon/SqrMelon.bat
@@ -1,1 +1,22 @@
-"%USERPROFILE%/AppData/Local/Programs/Python/Python311/python.exe" main.py
+@echo off
+setlocal enabledelayedexpansion
+
+rem Search for the most recent version of Python 3 in %LOCALAPPDATA%\Programs\Python.
+set "PYTHON_DIR=%LOCALAPPDATA%\Programs\Python"
+set "LATEST_VERSION=0"
+set "LATEST_PYTHON="
+
+for /D %%d in ("%PYTHON_DIR%\Python3*") do (
+    set "VERSION=%%~nxd"
+    set "VERSION=!VERSION:Python=!"
+    if !VERSION! gtr !LATEST_VERSION! (
+        set "LATEST_VERSION=!VERSION!"
+        set "LATEST_PYTHON=%%d"
+    )
+)
+
+if "%LATEST_PYTHON%"=="" (
+    echo No Python 3 installment found in  %PYTHON_DIR%.
+    exit /b 1
+)
+"%LATEST_PYTHON%\python.exe" main.py
